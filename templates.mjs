ea75b1ee15b99ce0ca57971f6fda1a41d66c2fec
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-02-08 19:22:07 +0500

    feat: add next-ts template

--- templates.mjs
+++ templates.mjs
@@ -6,7 +6,15 @@ const templates = [
         languages: ['typescript'],
         packageManagers: ['npm', 'yarn', 'pnpm'],
         type: 'standalone'
-    }
+    },
+    {
+        id: 'next-ts',
+        url: 'grayhatdevelopers/next-ts-multiplayer-game#main',
+        framework: 'nextjs',
+        languages: ['typescript'],
+        packageManagers: ['npm', 'yarn', 'pnpm'],
+        type: 'standalone'
+    }    
 ];
 
 export default templates
